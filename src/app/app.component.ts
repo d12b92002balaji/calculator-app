cdc44439970cbc8bed869bc379d3c0204dfbf871
Dheepanbalaji <dheepanbalajicse@gmail.com>
2021-09-14 18:51:57 +0530

    calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calcInAngular';
+  title = 'Welcome to angular calculator';
+  public 'num1':string;
+  public 'num2':string;
+  public 'num3':number;
+  
+  add(){
+    this.num3=parseInt(this.num1)+parseInt(this.num2);
+  }
+  sub(){
+    this.num3=parseInt(this.num1)-parseInt(this.num2);
+  }
+  mul(){
+    this.num3=parseInt(this.num1)*parseInt(this.num2)
+  
+  }
+  div(){
+    
+    this.num3=parseInt(this.num1)/parseInt(this.num2)
+
+  }
+
 }
